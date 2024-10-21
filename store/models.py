b54ab3cb719d537f4b5549723bb2702b61162fb6
TEACHER\ICT02_22 <admin@admin.com>
2024-10-21 17:45:21 +0900

    commit

--- store/models.py
+++ store/models.py
@@ -1,5 +1,8 @@
+from turtle import back
+from unicodedata import category
 from django.db import models
-import datatime
+from datetime import datetime
+
 
 
 # Create your models here.
@@ -9,7 +12,35 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
-class Customer
-class Product
-class Order
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0,decimal_places=2,max_digits=6) #9999.99
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
+    description = models.CharField(max_length=250,default='',blank=True, null=True)
+    image = models.ImageField(upload_to='upload/product')
 
+    def __str__(self):
+        return self.name
+
+#Coustomer orders
+class Order(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=20,default='',blank=True)
+    date = models.DateField(default = datetime.today)
+    status = models.BooleanField(default = False)
+
+    def __str__(self):
+        return self.product
