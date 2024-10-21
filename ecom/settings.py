343d94862f0a870bd7114748eee65fc395120f2b
TEACHER\ICT02_22 <admin@admin.com>
2024-10-21 14:30:55 +0900

    모델 추가 클래스

--- ecom/settings.py
+++ ecom/settings.py
@@ -106,6 +106,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+import os
+
+MEDIA_URL = 'media/'		# ex) /media/photo1.png
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
