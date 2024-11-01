e0cdfc4951be389515151283743b481ef149021a
TEACHER\ICT02_22 <admin@admin.com>
2024-11-01 17:27:01 +0900

    카트 구성

---
+++ cart/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+
+from cart import views
+
+urlpatterns = [
+    path("", views.cart_summary,name="cart_summary"),
+    path("add/", views.cart_add,name="cart_add"), 
+    path("delete/", views.cart_delete,name="cart_delete"), 
+    path("update/", views.cart_update,name="cart_update"),      
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+def cart_summary(reqeust):
+
+    return render(reqeust, "cart_summary.html",{})
+
+def cart_add(reqeust):
+    pass
+
+def cart_delete(reqeust):
+    pass
+
+def cart_update(reqeust):
+    pass
+
