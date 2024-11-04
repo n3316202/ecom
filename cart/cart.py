a46d5603c335a8c737981309998f3940b896e786
TEACHER\ICT02_22 <admin@admin.com>
2024-11-04 17:52:22 +0900

    commit

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,6 @@
+#https://ramyo564.github.io/django/session/
+# 아래코드 설명
+
 class Cart():
     def __init__(self,request):
         self.session = request.session
@@ -6,10 +9,18 @@ class Cart():
         cart = self.session.get('session_key')
 
         if 'session_key' not in request.session:
-            cart = self.session.session_key = {}
+            cart = self.session['session_key'] = {}
 
         #make suer cart is available
         self.cart = cart
 
+    def add(self,product):
+        product_id  = str(product_id)
 
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str()}
+        
+        self.session.modified = True
             
--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from urllib import response
+from django.shortcuts import get_object_or_404, render
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 def cart_summary(reqeust):
-
     return render(reqeust, "cart_summary.html",{})
 
 def cart_add(reqeust):
-    pass
+    cart  = Cart(reqeust)
+    if reqeust.POST.get('action') == 'POST':
+        #get stuff
+        product_id = int(reqeust.POST.get('prodcut_id'))
+        # lookup proudct in DB
+        prodcut = get_object_or_404(Product,id=product_id)
+
+        #save to session
+        cart.add(prodcut=prodcut)
+        response = JsonResponse({'Product Name': prodcut.name})
+        
+        return response
 
 def cart_delete(reqeust):
     pass
