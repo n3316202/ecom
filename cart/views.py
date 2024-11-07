06c5ba5ab119b8862ee6ed921ea4f625bc4f77e9
TEACHER\ICT02_22 <admin@admin.com>
2024-11-07 14:21:14 +0900

    commit

--- cart/views.py
+++ cart/views.py
@@ -9,8 +9,9 @@ def cart_summary(request):
     cart = Cart(request)
     cart_products = cart.get_prods()
     quantities = cart.get_quants
+    totals = cart.cart_total()
     print(quantities , "==============")
-    return render(request, "cart_summary.html",{"cart_products": cart_products,"quantities": quantities})
+    return render(request, "cart_summary.html",{"cart_products": cart_products,"quantities": quantities,"totals":totals})
 
 def cart_add(request):
     
