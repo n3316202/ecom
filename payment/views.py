2eebc1f8da9618cb644cae7d95fefefa7467e2db
TEACHER\ICT02_22 <admin@admin.com>
2024-11-11 15:39:14 +0900

    commit

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
+
+def checkout(request):
+    return render(request, "payment/checkout.html",{})
+
 # Create your views here.
 def payment_success(request):
     return render(request, "payment/payment_success.html",{})
