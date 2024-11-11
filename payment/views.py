bce506f7a79e680f9f9b319714f00ed1e3ff673d
TEACHER\ICT02_22 <admin@admin.com>
2024-11-11 17:43:59 +0900

    commit

--- payment/views.py
+++ payment/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from urllib import response
+from django.shortcuts import get_object_or_404, render
+from cart.cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+from django.contrib import messages
 
 
 
 def checkout(request):
-    return render(request, "payment/checkout.html",{})
+    # Get the cart
+    cart = Cart(request)
+    cart_products = cart.get_prods()
+    quantities = cart.get_quants()
+    totals = cart.cart_total()
+    print("아아",quantities)
+    return render(request, "payment/checkout.html",{"cart_products": cart_products,"quantities": quantities,"totals":totals})
 
+
+# Create your views here.
 # Create your views here.
 def payment_success(request):
     return render(request, "payment/payment_success.html",{})
